d3ae7061452b8dbe16043e0232ae001c959b57ba
Sergio <sgonzalezb@cifpfbmoll.eu>
2021-01-03 18:36:37 +0100

    refactor: chance option

--- python/yatzy.py
+++ python/yatzy.py
@@ -1,14 +1,15 @@
 class Yatzy:
 
+    def __init__(self, *dice):
+        self.dice = list(dice)
+
     @staticmethod
-    def chance(d1, d2, d3, d4, d5):
-        total = 0
-        total += d1
-        total += d2
-        total += d3
-        total += d4
-        total += d5
-        return total
+    def chance(*dice):
+        amount = 0
+        for side in dice:
+            amount += side
+        return amount
+        
 
     @staticmethod
     def yatzy(dice):
@@ -68,13 +69,7 @@ class Yatzy:
         return s
     
 
-    def __init__(self, d1, d2, d3, d4, _5):
-        self.dice = [0]*5
-        self.dice[0] = d1
-        self.dice[1] = d2
-        self.dice[2] = d3
-        self.dice[3] = d4
-        self.dice[4] = _5
+    
     
     def fours(self):
         sum = 0
