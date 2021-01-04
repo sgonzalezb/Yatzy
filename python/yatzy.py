f44a843fe0fec6b0af5762cde80eae1cbcd75cd1
Sergio <sgonzalezb@cifpfbmoll.eu>
2021-01-04 13:56:18 +0100

    refactor: all refactor code

--- python/yatzy.py
+++ python/yatzy.py
@@ -4,28 +4,24 @@ class Yatzy:
         self.dice = list(dice)
 
     @staticmethod
-    def chance(*dice):
+    def chance(*dice): 
         amount = 0
         for side in dice:
             amount += side
         return amount
         
-
     @staticmethod
     def yatzy(dice):
-        counts = [0]*(len(dice)+1)
-        for die in dice:
-            counts[die-1] += 1
-        for i in range(len(counts)):
-            if counts[i] == 5:
-                return 50
+        for side in dice:
+            if dice.count(side) == 5: 
+                return 50                   
+            break
         return 0
-    
+
     @staticmethod
     def ones(*dice):
         one = 1
-        return dice.count(one) * 1  #CUENTA LAS VECES QUE SE REPITE UN NÚMERO Y LO MULTIPLICA POR SU VALOR
-    
+        return dice.count(one) * 1  
 
     @staticmethod
     def twos(*dice):
@@ -37,144 +33,81 @@ class Yatzy:
         three = 3
         return dice.count(three) * 3
 
-    def fours(self):        ###NO ENTIENDO PORQUE HAY QUE PONER SELF// Y SI LE PONGO *DICE ASSERT ERROR
+    def fours(self):       
         four = 4
         return self.dice.count(four) * 4
 
     def fives(self):
         five = 5
         return self.dice.count(five) * 5
-    
 
     def sixes(self):
         six = 6
         return self.dice.count(six) * 6
     
     @staticmethod
-    def score_pair( d1,  d2,  d3,  d4,  d5):
-        counts = [0]*6
-        counts[d1-1] += 1
-        counts[d2-1] += 1
-        counts[d3-1] += 1
-        counts[d4-1] += 1
-        counts[d5-1] += 1
-        at = 0
-        for at in range(6):
-            if (counts[6-at-1] == 2):
-                return (6-at)*2
-        return 0
+    def score_pair(*dice):
+        amount = 0                                              
+        for side in dice:                                       
+            if dice.count(side) == 2 and amount < side * 2:     
+                amount = side * 2                               
+        return amount                                           
     
     @staticmethod
-    def two_pair( d1,  d2,  d3,  d4,  d5):
-        counts = [0]*6
-        counts[d1-1] += 1
-        counts[d2-1] += 1
-        counts[d3-1] += 1
-        counts[d4-1] += 1
-        counts[d5-1] += 1
-        n = 0
-        score = 0
-        for i in range(6):
-            if (counts[6-i-1] >= 2):
-                n = n+1
-                score += (6-i)
-                    
-        if (n == 2):
-            return score * 2
-        else:
-            return 0
-    
-    @staticmethod
-    def four_of_a_kind( _1,  _2,  d3,  d4,  d5):
-        tallies = [0]*6
-        tallies[_1-1] += 1
-        tallies[_2-1] += 1
-        tallies[d3-1] += 1
-        tallies[d4-1] += 1
-        tallies[d5-1] += 1
-        for i in range(6):
-            if (tallies[i] >= 4):
-                return (i+1) * 4
+    def two_pair(*dice):
+        amount = 0
+        repetidos = []
+        contador_pair = 0
+        for side in dice:
+            if dice.count(side) >= 2 and side not in repetidos: ##LISTO
+                amount += side * 2
+                repetidos.append(side)
+                contador_pair += 1
+            if contador_pair == 2:
+                return amount
         return 0
-    
 
     @staticmethod
-    def three_of_a_kind( d1,  d2,  d3,  d4,  d5):
-        t = [0]*6
-        t[d1-1] += 1
-        t[d2-1] += 1
-        t[d3-1] += 1
-        t[d4-1] += 1
-        t[d5-1] += 1
-        for i in range(6):
-            if (t[i] >= 3):
-                return (i+1) * 3
-        return 0
-    
+    def four_of_a_kind(*dice):
+        amount = 0
+        for side in dice:
+            if dice.count(side) >= 4: ##LISTO
+                amount = side * 4
+            return amount
 
     @staticmethod
-    def smallStraight( d1,  d2,  d3,  d4,  d5):
-        tallies = [0]*6
-        tallies[d1-1] += 1
-        tallies[d2-1] += 1
-        tallies[d3-1] += 1
-        tallies[d4-1] += 1
-        tallies[d5-1] += 1
-        if (tallies[0] == 1 and
-            tallies[1] == 1 and
-            tallies[2] == 1 and
-            tallies[3] == 1 and
-            tallies[4] == 1):
+    def three_of_a_kind(*dice):
+        amount = 0
+        for side in dice:
+            if dice.count(side) >= 3:   ##LISTO
+                amount = side * 3
+        return amount   
+
+    @staticmethod
+    def smallStraight(*dice):
+        if sorted(dice) == [1,2,3,4,5]:
             return 15
         return 0
-    
 
     @staticmethod
-    def largeStraight( d1,  d2,  d3,  d4,  d5):
-        tallies = [0]*6
-        tallies[d1-1] += 1
-        tallies[d2-1] += 1
-        tallies[d3-1] += 1
-        tallies[d4-1] += 1
-        tallies[d5-1] += 1
-        if (tallies[1] == 1 and
-            tallies[2] == 1 and
-            tallies[3] == 1 and
-            tallies[4] == 1
-            and tallies[5] == 1):
+    def largeStraight(*dice):
+        if sorted(dice) == [2,3,4,5,6]:
             return 20
         return 0
-    
 
     @staticmethod
-    def fullHouse( d1,  d2,  d3,  d4,  d5):
-        tallies = []
-        _2 = False
-        i = 0
-        _2_at = 0
-        _3 = False
-        _3_at = 0
+    def fullHouse(*dice):
+        amount = 0
+        double = False
+        triple = False
+        for side in dice:
+            if dice.count(side) == 2 and double is False:
+                amount += side * 2
+                double = True
+            if dice.count(side) == 3 and triple is False:
+                amount += side * 3
+                triple = True
+            if triple is True and double is True:
+                return amount
+        return 0
 
-        tallies = [0]*6
-        tallies[d1-1] += 1
-        tallies[d2-1] += 1
-        tallies[d3-1] += 1
-        tallies[d4-1] += 1
-        tallies[d5-1] += 1
-
-        for i in range(6):
-            if (tallies[i] == 2): 
-                _2 = True
-                _2_at = i+1
-            
-
-        for i in range(6):
-            if (tallies[i] == 3): 
-                _3 = True
-                _3_at = i+1
-            
-
-        if (_2 and _3):
-            return _2_at * 2 + _3_at * 3
-        else:
-            return 0
